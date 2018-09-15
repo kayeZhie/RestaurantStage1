999aebbb9a18fd3ff343663afe6fe04c348e4d49
Karren Zoleta <zoletakarren@gmail.com>
2018-09-15 12:59:46 -0700

    responsive index page

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiY2xvY2t3ZXJreiIsImEiOiJjamxnM3kzeHkxMDFwM3dxem1neHBhbHMyIn0.n4Mp1APXNj5ECtqriJhKLA',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
